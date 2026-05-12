306b5426e267e349c4ad53c9704ce2b8c86b0ff4
almasais <almasaalatiyah@gmail.com>
2026-05-12 15:31:07 +0300

    added the cloudkit

--- PawPin/Managers/CloudKitManager.swift
+++ PawPin/Managers/CloudKitManager.swift
@@ -5,4 +5,30 @@
 //  Created by lay on 24/11/1447 AH.
 //
 
+import Foundation
+import CloudKit
 
+class CloudKitManager {
+    
+    static let shared = CloudKitManager()
+    
+    private let database = CKContainer.default().publicCloudDatabase
+    
+    // SAVE POST
+    func saveCat(name: String, description: String) {
+        
+        let record = CKRecord(recordType: "CatPost")
+        
+        record["name"] = name
+        record["description"] = description
+        
+        database.save(record) { record, error in
+            
+            if let error = error {
+                print("ERROR SAVING:", error.localizedDescription)
+            } else {
+                print("SUCCESS")
+            }
+        }
+    }
+}
