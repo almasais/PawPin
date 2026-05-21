8768acdd6aa34e481e2a2c76f46c6c9c501549f3
Afnan <ahassan25@twq.idesrve.net>
2026-05-21 14:48:47 +0300

    Aipage

---
+++ PawPin/Views/AIAnalysisView.swift/AIPage.swift
@@ -0,0 +1,18 @@
+//
+//  AIPage.swift
+//  PawPin
+//
+//  Created by Afnan hassan on 01/12/1447 AH.
+//
+
+import SwiftUI
+
+struct AIPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AIPage()
+}
